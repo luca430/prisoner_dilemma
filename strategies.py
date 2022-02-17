888d5c3a21c7bd93e200370487f54324975bb1e7
SbarbatiR <riccardo.sbarbati@studenti.unipd.it>
2022-02-17 15:11:25 +0100

    added new strategies, created update 4

--- strategies.py
+++ strategies.py
@@ -28,7 +28,7 @@ def random(it,u,v,u2):
     if a < 0.5: return uc
     else: return ud
     
-def pavlov(it,u,v,u2):
+def grim(it,u,v,u2):
     if it==2:
         return uc
     else:
@@ -45,9 +45,57 @@ def f_tit_tat(it,u,v,u2):
         if u == ud and u2 == ud:
             return ud
         else: return uc
+        
+def sus_tit_tat(it,u,v,u2):
+    if it==2: return ud
+    else: return u
     
+def pavlov(it,u,v,u2):
+    if u == v: return uc
+    else: return ud
     
+def reactive_nice(it,u,v,u2):
+    y = 0.5
+    p = 0.7
+    if it == 2:
+        if npr.random() <= y: return uc
+        else: return ud
+    else:
+        if u == uc:
+            if npr.random() <= p: return uc
+            else: return ud
+        else:
+            if npr.random() <= 1-p: return uc
+            else: return ud
     
-    
-    
-    
+def reactive_bad(it,u,v,u2):
+    y = 0.5
+    p = 0.3
+    if it == 2:
+        if npr.random() <= y: return uc
+        else: return ud
+    else:
+        if u == uc:
+            if npr.random() <= p: return uc
+            else: return ud
+        else:
+            if npr.random() <= 1-p: return uc
+            else: return ud 
+            
+def hard_joss(it,u,v,u2):
+    p = 0.9
+    if it==2: return uc
+    else:
+        if u == uc:
+            if npr.random() <= p: return uc
+            else: return ud
+        else: return ud
+        
+def soft_joss(it,u,v,u2):
+    p = 0.9
+    if it==2: return uc
+    else:
+        if u == ud:
+            if npr.random() <= p: return ud
+            else: return uc
+        else: return uc
